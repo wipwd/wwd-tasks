dc18622c3614d3e616eadce0eb4a79499c509bb3
Joao Eduardo Luis <joao@wipwd.dev>
2020-11-20 18:28:54 -0100

    env: add firebase stuff to production env
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD6DHsoRCFrXG8n_hmDnBJwkS8Bz4sn0DY",
+    authDomain: "wwd-tasks.firebaseapp.com",
+    databaseURL: "https://wwd-tasks.firebaseio.com",
+    projectId: "wwd-tasks",
+    storageBucket: "wwd-tasks.appspot.com",
+    messagingSenderId: "531472695407",
+    appId: "1:531472695407:web:6f9d1963839808db930d9a",
+    measurementId: "G-H5WMDQJS9N"
+  }
 };
