35b766e4f1642c10d4918c322480942feab3d063
Joao Eduardo Luis <joao@wipwd.dev>
2020-11-28 16:30:01 -0100

    tasks: start storage on app start
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StorageService } from './services/storage-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wwd-tasks';
+
+  public constructor(
+    private _storage_svc: StorageService
+  ) {
+    console.log(`starting storage version ${this._storage_svc.STORE_VERSION}`);
+  }
 }
