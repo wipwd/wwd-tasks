b57afddf0cb78eba4f4a4e9ee967d6035e935e3a
Joao Eduardo Luis <joao@algazarra.lan>
2020-12-24 08:20:30 +0000

    settings: new view look and feel
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- src/app/settings/settings-dashboard/settings-dashboard.component.ts
+++ src/app/settings/settings-dashboard/settings-dashboard.component.ts
@@ -1,32 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { ProjectsService } from '../../services/projects-service.service';
 import { ENTER, COMMA } from '@angular/cdk/keycodes';
 import { FormControl } from '@angular/forms';
 import { MatChipInputEvent } from '@angular/material/chips';
-import {
-  StorageService,
-} from '../../services/storage-service.service';
 import { WWDTASKS_BUILD_COMMIT, WWDTASKS_BUILD_DATE } from '../../build-info';
-import * as triplesec from 'triplesec/browser/triplesec';
-import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
-import firebase from 'firebase/app';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
+interface SettingsEntryItem {
+  id: string;
+  label: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-settings-dashboard',
   templateUrl: './settings-dashboard.component.html',
-  styleUrls: ['./settings-dashboard.component.scss']
+  styleUrls: ['./settings-dashboard.component.scss'],
+  animations: [
+    trigger("InOutAnimation", [
+      state("in", style({transform: "translateX(0%)"})),
+      state("out", style({transform: "translateX(100%)"})),
+      transition("in <=> out", animate("200ms ease-in")),
+      transition(":enter", [
+        style({transform: "translateX(100%)"}),
+        animate("200ms ease-in", style({transform: "translateX(0%)"}))
+      ]),
+      transition(":leave", [
+        style({transform: "translateX(0%)"}),
+        animate("200ms ease-in", style({transform: "translateX(100%)"}))
+      ])
+    ]),
+    trigger("ExpandAnimation", [
+      state("expanded", style({width: "100%"})),
+      state("contracted", style({width: "250px"})),
+      transition("expanded => contracted", [
+        animate("200ms ease-in")
+      ]),
+      transition("contracted => expanded", [
+        animate("200ms ease-in")
+      ])
+    ])
+  ]
 })
 export class SettingsDashboardComponent implements OnInit {
 
   public chip_separator_codes: number[] = [ ENTER, COMMA ];
   public project_form_ctrl: FormControl = new FormControl();
 
+  public settings_items: SettingsEntryItem[] = [
+    { id: "projects", label: "Projects", icon: "book" },
+    { id: "importexport", label: "Import/Export Data", icon: "import_export" },
+    { id: "sync", label: "Device Sync", icon: "sync_alt" },
+    { id: "appinfo", label: "Application Information", icon: "info" }
+  ];
+
+  public selected_item: string = "projects";
+
+
   public constructor(
     private _projects_svc: ProjectsService,
-    private _firestore_auth: AngularFireAuth,
   ) { }
 
   public ngOnInit(): void { }
@@ -60,5 +94,12 @@ export class SettingsDashboardComponent implements OnInit {
 
   public getBuildDate(): string {
     return WWDTASKS_BUILD_DATE;
+  }
+
+  public selectedEntry(entry: SettingsEntryItem): void {
+    if (entry.id === this.selected_item) {
+      return;
+    }
+    this.selected_item = entry.id;
   }
 }
