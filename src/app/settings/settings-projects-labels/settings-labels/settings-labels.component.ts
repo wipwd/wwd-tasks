4165257c059b510f24fbd6c32e501c66da1b891b
Joao Eduardo Luis <joao@wipwd.dev>
2020-12-25 12:05:08 +0000

    settings/labels: rename and delete labels, proper.
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- src/app/settings/settings-projects-labels/settings-labels/settings-labels.component.ts
+++ src/app/settings/settings-projects-labels/settings-labels/settings-labels.component.ts
@@ -10,7 +10,10 @@ import { Label, LabelsMap, LabelsService } from '../../../services/labels-servic
 export class SettingsLabelsComponent implements OnInit {
 
   public label_add_form_ctrl: FormControl = new FormControl("");
+  public label_rename_form_ctrl: FormControl = new FormControl("");
+
   public labels: Label[] = [];
+  public edit_label_id: number = -1;
 
   public constructor(
     private _labels_svc: LabelsService
@@ -44,16 +47,41 @@ export class SettingsLabelsComponent implements OnInit {
       return;
     }
     this._labels_svc.remove(_label.name);
+
+    if (this.edit_label_id === _label.id) {
+      this.edit_label_id = -1;
+      this.label_rename_form_ctrl.setValue("");
+    }
   }
 
-  public renameLabel(_label: Label): void {
+  public toggleRenameLabel(_label: Label): void {
     if (!_label) {
       throw new Error("undefined label");
     } else if (!this.labels.includes(_label)) {
       throw new Error(`unknown label ${_label.name}`);
+    } else if (this.edit_label_id !== -1 && this.edit_label_id !== _label.id) {
+      return;
     }
 
-    // edit form
+    this.edit_label_id = (this.edit_label_id >= 0 ? -1 : _label.id);
+    if (this.edit_label_id >= 0) {
+      this.label_rename_form_ctrl.setValue(_label.name);
+    }
+  }
+
+  public submitLabelRename(_label: Label): void {
+    if (!_label || !this.labels.includes(_label)) {
+      return;
+    }
+    const value: string = this.label_rename_form_ctrl.value?.trim();
+    if (!value || value === "") {
+      return;
+    }
+    const old_name: string = _label.name;
+    const new_name: string = value;
+    this.edit_label_id = -1;
+    this.label_rename_form_ctrl.setValue("");
+    this._labels_svc.rename(old_name, new_name);
   }
 
 }
