f3eb4ef773227e39d208dfb7135c9ee2b42068b9
Joao Eduardo Luis <joao@wipwd.dev>
2020-12-25 12:05:45 +0000

    service/labels: fix typo on function name
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- src/app/services/labels-service.service.ts
+++ src/app/services/labels-service.service.ts
@@ -64,7 +64,7 @@ export class LabelsService {
     return this._storage_subject;
   }
 
-  public getInitSTate(): LabelsStorageDataItem {
+  public getInitState(): LabelsStorageDataItem {
     return this._getCurrentState();
   }
 
